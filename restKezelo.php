fce7c1c96dfd7e8d19ecf7fdd6e5240b30e3dcc3
Leventeszily <72456857620@szily.hu>
2025-03-11 13:26:39 +0100

    restvezerlo.php és egy kis javitas

--- restKezelo.php
+++ restKezelo.php
@@ -6,8 +6,7 @@ class RestKezelo
     public function sethttpFejlec($statusKod)
     {
         $statusUzenet = $this->gethttpStatusUzenet($statusKod);
-        header($this->httpVersion. " ". $statusKod 
-        . " ". $statusUzenet);
+        header($this->httpVersion. " ". $statusKod . " ". $statusUzenet);
         header("Content-Type: Application/json; charset=utf-8");
     }
     public function gethttpStatusUzenet($statusKod)
