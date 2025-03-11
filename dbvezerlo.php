bcf9342a94242892ebdda52ba39f4cd61319ee23
Leventeszily <72456857620@szily.hu>
2025-03-11 12:07:06 +0100

    dbvezerlo elkeszult

---
+++ dbvezerlo.php
@@ -0,0 +1,41 @@
+<?php
+
+class DBVezerlo {
+    private $conn = null;
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $database = "oscar";
+
+    function __construct() 
+    {
+        print("mysqli");
+        $conn = $this->connectDB();
+        if(!empty($conn))
+        {
+            $this->conn = $conn;
+        }
+    }
+    function connectDB()
+    {
+        $conn = nysqli_connect($this->host, $this->user, $this->password, $this->database);
+        return $conn;
+    }
+
+    function executeSelectQuery($querry)
+    {
+        $result = mysqli_query($this->conn, $query);
+        while($row=mysqli_fetch_assoc($result))
+        {
+            $resultset[] = $row;
+        }
+
+        if(!empty($resultset))
+        {
+            return $resultset;
+        }
+    }
+
+
+}
+?>
